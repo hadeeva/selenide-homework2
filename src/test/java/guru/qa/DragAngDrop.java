2c916e9e80f9f48729bc376408864e73f73871cb
Veronika <ayzeklark@gmail.com>
2024-01-20 02:14:00 +0300

    inits

--- src/test/java/guru/qa/DragAngDrop.java
+++ src/test/java/guru/qa/DragAngDrop.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAngDrop {
@@ -14,16 +15,16 @@ public class DragAngDrop {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
     @Test
     void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200,0).release().perform();
+        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200,0).release().perform();
 
-        //$("#column-b").dragAndDrop($("#column-a"));
+        $("#column-a").dragAndDrop(to($("#column-b")));
 
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
